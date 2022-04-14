6a65ecfa6e3c1e8f4f1f781527b22c4d34b4a5ee
Evelor13 <evelor8@gmail.com>
2022-04-15 01:46:07 +0400

    Добавлена таблица с картинками

--- app/views/index.php
+++ app/views/index.php
@@ -6,14 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <ul>
-        <?php
-            foreach($books as $book):
-                echo "книга -- {$book['name']} <br>";
-            endforeach;
-        ?>
-    </ul>
+    <body>
+        <ul>
+            <table border="1">
+                <tr>
+                    <th>id</th>
+                    <th>Книга</th>
+                    <th>Автор</th>
+                    <th>Год</th>
+                    <th>Обложка</th>
+                </tr>
+                <?php foreach ($books as $book): ?>
+                    <tr>
+                        <td>
+                            <?php
+                                echo "{$book['id']} <br>";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$book['name']} <br>";
+                        ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$book['fk_autor']} <br>";
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo "{$book['year']} <br>";
+                            ?>
+                        </td>
+                        <td>
+                            <img src="<?php 
+                                        echo "{$book['image']} <br>";
+                                      ?>"
+                                    width="100" height="150">
+                           
+                        </td>
+                </tr>
+                <?php endforeach;?>
+            </table>
+          </ul>
     <h1>Успех!</h1>
     <img src="https://shishkino.info/wp-content/uploads/2017/08/blackbooks3.jpg" alt="">
 
