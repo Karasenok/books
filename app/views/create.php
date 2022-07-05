9f7357c6c68eb8345e520e4c8393337f885455b0
evelor <evelor@mail.ru>
2022-07-06 00:32:37 +0400

    сохранение книг в бд

--- app/views/create.php
+++ app/views/create.php
@@ -4,24 +4,29 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-<body>  
-      <form action="/books/store" method="POST">
-    
-            <label for="name">Название книги:</label>
-            <input type="text" name="book_name">
-            <br></br>
-            <label for="author">Автор книги:</label>
-            <input type="text" name="author">
-            <br></br>
-            <label for="date">Год:</label>
-            <input type="date" name="date">
-            <br></br>
-            <label for="image">Обложка:</label>
-            <form enctype="multipart/form-data" method="post">
-            <p><input type="file" name="userfile">
-            <br></br>
-            <button type="submit">Отправить</button>
+<body>
+<form action="/books/store" method="POST">
 
-      </form>  
+    <label>
+        Название книги:
+        <input type="text" name="book_name">
+    </label>
+    <br><br>
+    <label>
+        Автор книги:
+        <input type="text" name="author">
+    </label>
+    <br><br>
+    <label>Год:
+        <input type="date" name="date">
+    </label>
+    <br><br>
+    <label>Обложка:
+        <input type="text" name="userfile">
+    </label>
+    <br><br>
+    <button type="submit" >Отправить</button>
+
+</form>
 </body>
 <!--<p>Потому что я пуджжжжж пуджжжжж пуджжжжж пуджжжжж пуджжжжж пуджжжжжжж</p>-->
